5ee84deda09fd5883d38f08e2dd2cb2c673bf56d
chz <chzaaow@gmail.com>
2026-07-22 17:03:51 +0800

    add: 0110

--- 0110-balanced-binary-tree/BalancedBinaryTree.java
+++ 0110-balanced-binary-tree/BalancedBinaryTree.java
@@ -37,7 +37,26 @@
  */
 public class BalancedBinaryTree {
     public boolean isBalanced(TreeNode root) {
-        return check(root)[0]==1;
+//        return check(root)[0]==1;
+        return heightDiff(root) != -1;
+    }
+
+    public int heightDiff(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int hleft = heightDiff(root.left);
+        if (hleft == -1)
+            return -1;
+        int hright = heightDiff(root.right);
+        if (hright == -1)
+            return -1;
+
+        if (Math.abs(hleft - hright) > 1) {
+            return -1;
+        }
+        return 1 + Math.max(hleft, hright);
     }
     private int[] check(TreeNode node) {
         // 空节点：平衡且高度为0
